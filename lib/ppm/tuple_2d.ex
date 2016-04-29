6e20a0769c0e6a679dd18e0e6e1ec84ff658135e
Matthew Keibler <matthew.keibler@gmail.com>
2016-04-29 17:20:40 -0400

    cleaned up Tuple2D

--- lib/ppm/tuple_2d.ex
+++ lib/ppm/tuple_2d.ex
@@ -6,10 +6,19 @@ defmodule Ppm.Tuple2D do
   """
 
   def create(width, height, initial \\ 0) when is_number(width) and width > 0 and is_number(height) and height > 0 do
-    (for y <- 1..height, do: (for x <- 1..width, do: initial) |> List.to_tuple) |> List.to_tuple
+    initial_row = construct_tuple(initial, width)
+    construct_tuple(initial_row, height)
   end
 
   def set(tuple, x, y, value) when is_number(x) and x >= 0 and is_number(y) and y >= 0 do
-    put_elem(tuple, y, put_elem(elem(tuple, y), x, value))
+    row = put_elem(elem(tuple, y), x, value)
+    put_elem(tuple, y, row)
+  end
+
+  defp construct_tuple(value, length) do
+    [value]
+    |> Stream.cycle
+    |> Enum.take(length)
+    |> List.to_tuple
   end
 end
